f4ba400de3666755fb6ea22e513d94cfa1540670
Alex <sannikov@wisc.edu>
2019-10-19 22:25:38 -0500

    add more routes

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-  res.render("index");
+   res.render("index");
 });
 
 app.get("/about", function(req, res) {
    res.render("about");
- });
+});
+
+app.get("/suggestions", function(req, res) {
+   res.render("suggestions");
+});
+
+app.get("/login", function(req, res) {
+   res.render("login");
+});
+
+app.get("/signup", function(req, res) {
+   res.render("signup");
+});
+
 
 app.listen(3000, function() {
-  console.log("Server has started.");
+   console.log("Server has started.");
 });
